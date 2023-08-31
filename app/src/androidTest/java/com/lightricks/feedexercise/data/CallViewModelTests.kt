99ffa68892d211f4ae3d5a7cc5121750ec6cdd91
ilbender <ibender@lightricks.com>
2023-08-31 10:11:06 +0300

    created tests to test both repo's ability to save data and model actually getting the data from the repo. can pass mockApi in the constructor.

---
+++ app/src/androidTest/java/com/lightricks/feedexercise/data/CallViewModelTests.kt
@@ -0,0 +1,44 @@
+package com.lightricks.feedexercise.data
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.lightricks.feedexercise.database.FeedDatabase
+import com.lightricks.feedexercise.database.UserProject
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CallViewModelTests {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val hardCodedList : List<UserProject> = listOf(
+        UserProject("01E18PGE1RYB3R9YF9HRXQ0ZSD","UnleashThePowerOfNatureThumbnail.jpg",true),
+        UserProject("01DX1RB94P35Q1A2W6AA5XCQZ9","AccountingTravisThumbnail.jpg",true),
+        UserProject("01EAEFVPZ6MFJEMCA8XB06HB01","yeti-thumbnail.jpg",true),
+        UserProject("01DX1RB965Z96AD283559NJT9T","BusinessDevDefaultThumbnail.jpg",true),
+        UserProject("01EAEFVXVT6VS2R24GR7Q40XE0","start-living-thumbnail.jpg",true),
+        UserProject("01DX1RB94MBAKC6ECYGMKVJT4B","Beach4JulyThumbnail.jpg",false),
+        UserProject("01EC34AY3EQTA5294ZRHC6FQMJ","sliderevealboundingbox-happy-independence-day-thumbnail.jpg",true),
+        UserProject("01DX1RB923YYSRNFQF90TJNP03","Recipes4JulyThumbnail.jpg",true),
+        UserProject("01EBX3ZC3HZEXJYKEQ8SZAMGDY","independence-day-celebration-thumbnail.jpg",true),
+        UserProject("01E7674TVS66G1AGGMDE3YJVD7","happy-valentine's-day-thumbnail.jpg",true)
+    )
+
+    lateinit var viewModelTest : ViewModelTest
+
+    @Before
+    fun setup(){
+        val db = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            FeedDatabase::class.java).build()
+        val feedRepository = FeedRepository(MockApiService,db)
+        this.viewModelTest = ViewModelTest(db,feedRepository,hardCodedList)
+    }
+    @Test
+    fun testFeedRepoSave(){
+        this.viewModelTest.testViewModelData()
+    }
+}
--- app/src/androidTest/java/com/lightricks/feedexercise/data/FeedRepositoryTest.kt
+++ app/src/androidTest/java/com/lightricks/feedexercise/data/FeedRepositoryTest.kt
@@ -1,6 +1,5 @@
 package com.lightricks.feedexercise.data
 
-
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -14,21 +13,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class FeedRepositoryTest {
-    lateinit var db : FeedDatabase
-    lateinit var feedRepository: FeedRepository
-    val hardCodedList : List<UserProject> = listOf(
-        UserProject("01E18PGE1RYB3R9YF9HRXQ0ZSD","UnleashThePowerOfNatureThumbnail.jpg",true),
-        UserProject("01DX1RB94P35Q1A2W6AA5XCQZ9","AccountingTravisThumbnail.jpg",true),
-        UserProject("01EAEFVPZ6MFJEMCA8XB06HB01","yeti-thumbnail.jpg",true),
-        UserProject("01DX1RB965Z96AD283559NJT9T","BusinessDevDefaultThumbnail.jpg",true),
-        UserProject("01EAEFVXVT6VS2R24GR7Q40XE0","start-living-thumbnail.jpg",true),
-        UserProject("01DX1RB94MBAKC6ECYGMKVJT4B","Beach4JulyThumbnail.jpg",false),
-        UserProject("01EC34AY3EQTA5294ZRHC6FQMJ","sliderevealboundingbox-happy-independence-day-thumbnail.jpg",true),
-        UserProject("01DX1RB923YYSRNFQF90TJNP03","Recipes4JulyThumbnail.jpg",true),
-        UserProject("01EBX3ZC3HZEXJYKEQ8SZAMGDY","independence-day-celebration-thumbnail.jpg",true),
-        UserProject("01E7674TVS66G1AGGMDE3YJVD7","happy-valentine's-day-thumbnail.jpg",true)
-    )
+class FeedRepositoryTest(private var db : FeedDatabase,
+                         private var feedRepository: FeedRepository,
+                         private var userProjectList : List<UserProject>) {
+
     @Before
     fun init(){
         this.db = Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(),
@@ -44,11 +32,10 @@ class FeedRepositoryTest {
             .observeOn(AndroidSchedulers.mainThread())
             .doOnNext{userProjects ->
                 fetchedList = userProjects
-                Assert.assertEquals(fetchedList,hardCodedList)
+                Assert.assertEquals(fetchedList,userProjectList)
             }
             .subscribe()
     }
-
 }
 
 
